08dfdceaa055e9437dbad02e1f981484f4501e6e
Neal Terrell <nealdt@gmail.com>
2024-09-12 09:11:31 -0700

    Expanded tests.

--- Othello/test/Othello.Test/Tests.cs
+++ Othello/test/Othello.Test/Tests.cs
@@ -14,7 +14,9 @@ namespace Othello.Tests {
 			foreach (var pos in BoardPosition.GetRectangularPositions(8, 8)) {
 				// (3, 3) and (4, 4) are white (player 2).
 				if (pos is { Row: 3, Col: 3 } or { Row: 4, Col: 4 }) {
-					board.GetPlayerAtPosition(pos).Should().Be(2);
+					//Assert.True(board.GetPlayerAtPosition(pos) == 2, "white has two pieces at the start");
+
+					board.GetPlayerAtPosition(pos).Should().Be(2, "white has two pieces at the start");
 				}
 				// (3, 4) and (4, 3) are black (player 1).
 				else if (pos is { Row: 3, Col: 4 } or { Row: 4, Col: 3 }) {
@@ -31,7 +33,7 @@ namespace Othello.Tests {
 		public void NewValue() {
 			// New board Value is 0.
 			OthelloBoard board = new OthelloBoard();
-			board.CurrentAdvantage.Advantage.Should().Be(0, "New board value is 0");
+			board.CurrentAdvantage.Advantage.Should().Be(0, "New board advantage is 0");
 		}
 
 		[Fact]
@@ -42,6 +44,45 @@ namespace Othello.Tests {
 		}
 
 		[Fact]
+		public void StartingMoves() {
+			OthelloBoard b = new OthelloBoard();
+			var possMoves = b.GetPossibleMoves();
+			possMoves.Should().HaveCount(4).And.BeEquivalentTo(
+				[new BoardPosition(2, 3), new BoardPosition(3, 2), new BoardPosition(4, 5), 
+				 new BoardPosition(5, 4)]
+			);
+		}
+
+		[Fact]
+		public void OneDirectionOneFlip() {
+			OthelloBoard b = new OthelloBoard();
+			b.ApplyMove(new(3, 2));
+			b.GetPlayerAtPosition(new(3, 2)).Should().Be(1);
+			b.GetPlayerAtPosition(new(3, 3)).Should().Be(1);
+			b.GetPlayerAtPosition(new(3, 4)).Should().Be(1);
+			b.GetPlayerAtPosition(new(4, 3)).Should().Be(1);
+			b.GetPlayerAtPosition(new(4, 4)).Should().Be(2);
+		}
+
+		[Fact]
+		public void OneDirectionManyFlips() {
+			OthelloBoard b = new OthelloBoard();
+			b.ApplyMove(new(3, 2));
+			b.ApplyMove(new(4, 2));
+			b.ApplyMove(new(5, 2));
+			
+			b.GetPlayerAtPosition(new(4, 2)).Should().Be(1);
+			b.GetPlayerAtPosition(new(4, 3)).Should().Be(1);
+			b.GetPlayerAtPosition(new(4, 4)).Should().Be(2);
+
+			b.ApplyMove(new(4, 1));
+			b.GetPlayerAtPosition(new(4, 1)).Should().Be(2);
+			b.GetPlayerAtPosition(new(4, 2)).Should().Be(2);
+			b.GetPlayerAtPosition(new(4, 3)).Should().Be(2);
+			b.GetPlayerAtPosition(new(4, 4)).Should().Be(2);
+		}
+
+			[Fact]
 		public void MultiDirectionMove() {
 			// Value and board updated correctly after doing a multi-directional move.
 			OthelloBoard board = new OthelloBoard();

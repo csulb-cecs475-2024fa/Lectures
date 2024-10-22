f38da96fa2f9a308999857ed8e470e2608266f35
Neal Terrell <neal.terrell@csulb.edu>
2024-10-21 22:12:50 -0700

    Othello ported to Avalonia

---
+++ Avalonia/Othello/src/Cecs475.Othello.Application/OthelloView.axaml.cs
@@ -0,0 +1,52 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using System;
+
+namespace Cecs475.Othello.AvaloniaApp;
+
+public partial class OthelloView : UserControl
+{
+	public static SolidColorBrush RED_BRUSH = new SolidColorBrush(Colors.Red);
+	public static SolidColorBrush GREEN_BRUSH = new SolidColorBrush(Colors.Green);
+
+	public OthelloView()
+    {
+        InitializeComponent();
+    }
+
+	private void Panel_PointerEntered(object sender, PointerEventArgs e) {
+		if (sender is not Control b) {
+			throw new ArgumentException(nameof(sender));
+		}
+		var square = (OthelloSquare)b.DataContext!;
+		var vm = (OthelloViewModel)Resources["vm"]!;
+		if (vm.PossibleMoves.Contains(square.Position)) {
+			square.IsHighlighted = true;
+		}
+	}
+
+	private void Panel_PointerExited(object sender, PointerEventArgs e) {
+		if (sender is not Control b) { throw new ArgumentException(nameof(sender)); }
+		var square = (OthelloSquare)b.DataContext!;
+		square.IsHighlighted = false;
+	}
+
+	public OthelloViewModel Model {
+		get { return (OthelloViewModel)this.Resources["vm"]!; }
+	}
+
+	private void Panel_PointerReleased(object? sender, Avalonia.Input.PointerReleasedEventArgs e) {
+		if (sender is not Control b) {
+			throw new ArgumentException(nameof(sender));
+		}
+		var square = (OthelloSquare)b.DataContext!;
+		var vm = (OthelloViewModel)Resources["vm"]!;
+		if (vm.PossibleMoves.Contains(square.Position)) {
+			vm.ApplyMove(square.Position);
+			square.IsHighlighted = false;
+		}
+	}
+}
---
+++ Avalonia/Othello/src/Cecs475.Othello.Model/OthelloMove.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cecs475.Othello.Model {
+	/// <summary>
+	/// Represents a single move that can be or has been applied to an OthelloBoard object.
+	/// </summary>
+	public class OthelloMove : IEquatable<OthelloMove> {
+		public int Player { get; set; }
+
+		/// <summary>
+		/// The position of the move.
+		/// </summary>
+		public BoardPosition Position { get; }
+
+		/// <summary>
+		/// Initializes a new OthelloMove instance representing the given board position.
+		/// </summary>
+		public OthelloMove(BoardPosition pos) {
+			Position = pos;
+		}
+
+		public OthelloMove(int player, BoardPosition pos) {
+			Player = player;
+			Position = pos;
+		}
+
+		public override bool Equals(object obj) {
+			return Equals(obj as OthelloMove);
+		}
+
+		/// <summary>
+		/// Returns true if the two objects have the same position.
+		/// </summary>
+		public bool Equals(OthelloMove other) {
+			return other != null && Position.Equals(other.Position);
+		}
+		
+		/// <summary>
+		/// True if the move represents a "pass".
+		/// </summary>
+		public bool IsPass =>
+			Position.Row == -1 && Position.Col == -1;
+
+
+		// For debugging.
+		public override string ToString() {
+			return Position.ToString();
+		}
+
+		public override int GetHashCode() {
+			var hashCode = 1257125194;
+			hashCode = hashCode * -1521134295 + Player.GetHashCode();
+			hashCode = hashCode * -1521134295 + EqualityComparer<BoardPosition>.Default.GetHashCode(Position);
+			return hashCode;
+		}
+	}
+}
